9105743e6a5df5ed20ac0813fb951c2de460fa7d
YC000 <YC000@users.noreply.github.com>
2019-01-29 16:51:39 -0500

    finished task 3

--- proj1_data_loading.py
+++ proj1_data_loading.py
@@ -8,13 +8,13 @@ from datetime import datetime
 #################################
 
 # task 1
-
 with open("proj1_data.json") as fp:
     data = json.load(fp)
-    
-training_set = data[0:10000]
-validation_set = data[10000:11000]
-testing_set = data[11000:12000]
+fp.close()
+
+with open("proj1_data.json") as fp:
+    data2 = json.load(fp)
+fp.close()
 
 def pre_process_text(text,frequency_map):
     precessed_list = text.lower().split()
@@ -41,7 +41,9 @@ def pre_process(data_set, nb_of_top_words=160):
     #print(frequency_map[0:160])
     #first get the top 160 item of the map, then get the keys into a list
     most_frequent_word = [i[0] for i in frequency_map[0:nb_of_top_words]]
-    # print(most_frequent_word)
+    
+#    print(most_frequent_word)
+    
     for item in data_set:
         x_counts = [0.0]*nb_of_top_words
         for word in item['text']:
@@ -52,16 +54,21 @@ def pre_process(data_set, nb_of_top_words=160):
 
     return data_set
 
-def getXandY(data,with_text_feature=1):
+def getXandY(data, with_text_feature=1, with_extra_features=0):
     x_set = []
     y_set = []
     for item in data:
-        if (with_text_feature == 1):
-            x_others = [1, np.log(item['children'] + 1), item['controversiality']]#, np.power(item['children'],2)] # 2 other features + bias
+        if (with_text_feature == 1 and with_extra_features == 1):
+            x_others = [1, np.log(item['children'] + 1), item['controversiality']] # 2 other features + bias
             x_set.append(x_others + item['w_counts'])  #add each data set
-        else:
+        elif (with_text_feature == 1 and with_extra_features == 0): # only text feature
+            x_others = [1] # bias
+            x_set.append(x_others + item['w_counts'])  #add each data set
+        elif (with_text_feature == 0 and with_extra_features == 0): # only 3 simple features
             x_set.append([1, item['children'], item['controversiality'], item['is_root']])
+            
         y_set.append([item['popularity_score']])
+        
     return np.array(x_set), np.array(y_set)
 
 # task 2
@@ -76,7 +83,7 @@ def w_closed(X,Y):
     return w
 
 # epsilon should be <= 10^-6, eta < 10^-5, beta < 0.001
-def w_gradient(X,Y,eta=1e-06,beta=1e-05,e=1e-6):
+def w_gradient(X,Y,eta=1e-06,beta=0.0001,e=1e-6):
     dim = np.array(X).shape[1]
 #    Xarg = np.insert(X,dim,1,axis=1) # include bias
 
@@ -87,8 +94,8 @@ def w_gradient(X,Y,eta=1e-06,beta=1e-05,e=1e-6):
     xtx = np.dot(X.T, X)
     xty = np.dot(X.T, Y)
     
-#    # test convergence
-#    past_diff = 10
+    # test convergence
+    past_diff = 10
     
     i = 1
     while np.linalg.norm(diff,2) > e:
@@ -99,69 +106,106 @@ def w_gradient(X,Y,eta=1e-06,beta=1e-05,e=1e-6):
         weight = weight - diff
         #print(np.linalg.norm(diff,2))
         
-#        # test convergence
-#        if (i==1):
-#            past_diff = np.linalg.norm(diff,2)
-##            print(np.linalg.norm(diff,2))
-#        if (i == 2 and past_diff <= np.linalg.norm(diff,2)):
-##            print(np.linalg.norm(diff,2))
-#            print("in 2nd if")
-#            return np.ones((dim,1))
-##         print(np.linalg.norm(err,2))
+        # test convergence
+        if (i==1):
+            past_diff = np.linalg.norm(diff,2)
+#            print(np.linalg.norm(diff,2))
+        if (i == 2 and past_diff <= np.linalg.norm(diff,2)):
+#            print(np.linalg.norm(diff,2))
+            print("   DO NOT CONVERGE")
+            return np.ones((dim,1))
+#         print(np.linalg.norm(err,2))
 
         i+=1
-#    print(i)
+    print("   ", i," iterations")
     return weight
 
 def MSE(X,w,y):
     xw = X @ w
     return ((y - xw).T @ (y-xw) / len(y))[0][0]
 
-#print(w_closed(X,Y))
-#print(w_gradient(X,Y))
+# task 3
 
-# w_gradient(X2,Y2)
+training_set = data[0:10000]
+validation_set = data[10000:11000]
+testing_set = data[11000:12000]
+
+training_set2 = data2[0:10000]
+validation_set2 = data2[10000:11000]
+testing_set2 = data2[11000:12000]
 
 #print ("-----------------------------------------------------------------")
+#training_set = pre_process(training_set)
+#validation_set = pre_process(validation_set)
+#
+#x_train, y_train = getXandY(training_set,1,1)
+#x_valid, y_valid = getXandY(validation_set,1,1)
 #
 #weight_c = w_closed(x_train,y_train)
 #
-#print("weight in closed form is: \n", weight_c)
+##print("weight in closed form is: \n", weight_c)
 #print("training MSE for closed form is: \n", MSE(x_train, weight_c, y_train))
 #print("validating MSE for closed form is: \n", MSE(x_valid, weight_c, y_valid))
 #
-#weight_g = w_gradient(x_train,y_train)
-#print("weight gradient is: \n", weight_g)
+#weight_g = w_gradient(x_train,y_train,1e-06,0.0001,1e-6)
+##print("weight gradient is: \n", weight_g)
 #print("training MSE for gradient is: \n", MSE(x_train, weight_g, y_train))
 #print("validating MSE for gradient is: \n", MSE(x_valid, weight_g, y_valid))
 
-# task 3
-print ("TASK 3 -----------------------------------------------------------------")
-training_set = pre_process(training_set,60)
-validation_set = pre_process(validation_set,60)
+print ("TASK 3 -------------------------------------------------------------")
+# get 60 words training set
+training_set60 = pre_process(training_set,60)
+validation_set60 = pre_process(validation_set,60)
 
-print("Testing with 3 simple features")
-# get x and y
-x_train, y_train = getXandY(training_set,0)
-x_valid, y_valid = getXandY(validation_set,0)
+# get 160 words
+training_set160 = pre_process(training_set2,160)
+validation_set160 = pre_process(validation_set2,160)
+testing_set160 = pre_process(testing_set2,160)
 
+# get x and y with no text features
+x_train, y_train = getXandY(training_set60,0,0)
+x_valid, y_valid = getXandY(validation_set60,0,0)
+
+# get x and y with 60 text features only
+x_train60, y_train60 = getXandY(training_set60,1,0)
+x_valid60, y_valid60 = getXandY(validation_set60,1,0)
+
+# get x and y with 160 text features
+x_train160, y_train160 = getXandY(training_set160,1,0)
+x_valid160, y_valid160 = getXandY(validation_set160,1,0)
+
+# get x and y with 160 text features + 2 extra features
+x_train162, y_train162 = getXandY(training_set160,1,1)
+x_valid162, y_valid162 = getXandY(validation_set160,1,1)
+
+print("********** Testing with 3 simple features **********")
 startTime = datetime.now()
-w_closed_train_3Simple = w_closed(x_train,y_train)
+w_closed_3Simple = w_closed(x_train,y_train)
 endTime = datetime.now()
-print("Closed form took: ", endTime-startTime, "minutes")
+print("closed form: ")
+print("   runtime: ", endTime-startTime, "minutes")
+print("   MSE train:", MSE(x_train, w_closed_3Simple, y_train))
+print("   MSE valid: ", MSE(x_valid, w_closed_3Simple, y_valid))
 
-startTime = datetime.now()
-w_gd_train_3Simple = w_gradient(x_train,y_train)
-endTime = datetime.now()
-print("Gradient took: ", endTime-startTime, "minutes")
+print()
 
+# gradient using different learning rates
+eta = [10e-05,10e-06,10e-07]
+beta = [0.001,10e-04,10e-05,10e-06]
 
+for n0 in eta:
+    for b in beta:
+        startTime = datetime.now()
+        w_gd_3Simple = w_gradient(x_train,y_train,n0,b)
+        endTime = datetime.now()
+        print('gradient with eta = ',n0, ' beta = ', b, ': ')
+        print("   runtime: ", endTime-startTime, "minutes")
+        print("   MSE train: ", MSE(x_train, w_gd_3Simple, y_train))
+        print("   MSE valid: ", MSE(x_valid, w_gd_3Simple, y_valid))
 
+print()
 
-
-
-'''
-# to test which values to give to hyperparameters
+print('********** test which values to give to hyperparameters with 162 features **********')
 mse_t_a = []
 mse_v_a = []
 best_mse_train = 1.14
@@ -174,9 +218,9 @@ for n0 in eta:
     for b in beta:
         for e in epsilon:
             print(n0,b,e)
-            w_g = w_gradient(x_train,y_train,n0,b,e)
-            mse_train = MSE(x_train, w_g, y_train)
-            mse_valid = MSE(x_valid, w_g, y_valid)
+            w_g = w_gradient(x_train162,y_train162,n0,b,e)
+            mse_train = MSE(x_train162, w_g, y_train162)
+            mse_valid = MSE(x_valid162, w_g, y_valid162)
             if (mse_train < best_mse_train):
                 best_mse_train = mse_train
                 mse_t_a.append((n0,b,e,mse_train))
@@ -184,13 +228,69 @@ for n0 in eta:
                 best_mse_valid = mse_valid
                 mse_v_a.append((n0,b,e,mse_valid))
 
-print("best train mse", mse_t_a)
-print("best valid mse", mse_v_a)
+print("best 2 train mse", mse_t_a[-1], mse_t_a[-2])
+print("best 2 valid mse", mse_v_a[-1], mse_t_a[-2])
 
-for (n0,b,e,mse) in mse_t_a:
-    for (v_n0,v_b,v_e,v_mse) in mse_v_a:
-        if n0==v_n0 and b==v_b and e==v_e:
-            print(n0,b,e,mse)
-            print(v_n0,v_b,v_e,v_mse)
-'''
-#end
+print()
+
+print('********** Compare 3 models using closed-form **********')
+print('model with only 3 simple features and no text features: ')
+startTime = datetime.now()
+w_closed_3Simple = w_closed(x_train,y_train)
+endTime = datetime.now()
+print("3 simple features closed form: ")
+print("   runtime: ", endTime-startTime, "minutes")
+print("   MSE train:", MSE(x_train, w_closed_3Simple, y_train))
+print("   MSE valid: ", MSE(x_valid, w_closed_3Simple, y_valid))
+
+print()
+
+print('model with only the top-60 words: ')
+startTime = datetime.now()
+w_closed_top60 = w_closed(x_train60,y_train60)
+endTime = datetime.now()
+print("top-60 words closed form: ")
+print("   runtime: ", endTime-startTime, "minutes")
+print("   MSE train:", MSE(x_train60, w_closed_top60, y_train60))
+print("   MSE valid: ", MSE(x_valid60, w_closed_top60, y_valid60))
+
+print()
+
+print('model with the full 160 word occurence: ')
+startTime = datetime.now()
+w_closed_top160 = w_closed(x_train160,y_train160)
+endTime = datetime.now()
+print("top-160 words closed form: ")
+print("   runtime: ", endTime-startTime, "minutes")
+print("   MSE train:", MSE(x_train160, w_closed_top160, y_train160))
+print("   MSE valid: ", MSE(x_valid160, w_closed_top160, y_valid160))
+
+print()
+
+print('********** adding two new features **********')
+
+print("top-160 words + 2 features closed form: ")
+startTime = datetime.now()
+w_closed_162 = w_closed(x_train162,y_train162)
+endTime = datetime.now()
+print("   runtime: ", endTime-startTime, "minutes")
+print("   MSE train:", MSE(x_train162, w_closed_162, y_train162))
+print("   MSE valid: ", MSE(x_valid162, w_closed_162, y_valid162))
+
+print()
+
+print("top-160 words + 2 features gradient descent: ")
+startTime = datetime.now()
+w_gd_162 = w_gradient(x_train162,y_train162)
+endTime = datetime.now()
+print("   runtime: ", endTime-startTime, "minutes")
+print("   MSE train:", MSE(x_train162, w_gd_162, y_train162))
+print("   MSE valid: ", MSE(x_valid162, w_gd_162, y_valid162))
+
+print()
+
+print("********** run 162 features' model on test set **********")
+x_test162, y_test162 = getXandY(testing_set160,1,1)
+
+print("the weight is: \n", w_closed_162)
+print("MSE test: ", MSE(x_test162, w_closed_162, y_test162))
